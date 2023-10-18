a231f7eb8ba1b4115216fac1a1b972b535f710fb
arha06001 <arha06001@E-PW0234JA>
2023-10-18 10:32:01 +0200

    Add project files.

---
+++ Inlämningsuppgif 4.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Skriv in ett heltal");
+        int tal = int.Parse(Console.ReadLine());
+
+        int störst = tal;
+
+        string svar = "";
+
+        do
+        {
+            Console.WriteLine("Vill du skriva in ett till heltal? (j/n)");
+            svar = Console.ReadLine();
+
+            if (svar == "n")
+            {
+                break;
+            }
+
+            Console.WriteLine("Skriv in ett till heltal");
+            int nyttTal = int.Parse(Console.ReadLine());
+
+            if (nyttTal > störst)
+            {
+                störst = nyttTal;
+            }
+
+            Console.WriteLine($"Det största talet du skrev in var {störst}");
+        } while (svar != "n");
+    }
+}
